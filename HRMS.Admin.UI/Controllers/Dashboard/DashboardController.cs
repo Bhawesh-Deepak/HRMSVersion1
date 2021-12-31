bc9c5c8b500e454b787e3c8c0c19e73900c54784
Bhawesh Deepak <Bhawesh Deepak@DESKTOP-KB2SSTK>
2021-12-31 04:03:57 -0800

    Sunsidry

--- HRMS.Admin.UI/Controllers/Dashboard/DashboardController.cs
+++ HRMS.Admin.UI/Controllers/Dashboard/DashboardController.cs
@@ -1,4 +1,6 @@
-﻿using HRMS.Core.Helpers.CommonHelper;
+﻿using HRMS.Core.Entities.Organisation;
+using HRMS.Core.Helpers.CommonHelper;
+using HRMS.Services.Repository.GenericRepository;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,9 +11,16 @@ namespace HRMS.Admin.UI.Controllers.Dashboard
 {
     public class DashboardController : Controller
     {
+        private readonly IGenericRepository<Subsidiary, int> _ISubsidiaryRepository;
+        
+        public DashboardController(IGenericRepository<Subsidiary, int> subsidryRepository)
+        {
+            _ISubsidiaryRepository = subsidryRepository;
+        }
         public async Task<IActionResult> Index()
         {
-            return await Task.Run(() => View(ViewHelper.GetViewPathDetails("Dashboard", "Dashboard")));
+            var model = await _ISubsidiaryRepository.GetAllEntities(x => x.IsActive && !x.IsDeleted);
+            return await Task.Run(() => View(ViewHelper.GetViewPathDetails("Dashboard", "Dashboard"), model.Entities));
         }
     }
 }
