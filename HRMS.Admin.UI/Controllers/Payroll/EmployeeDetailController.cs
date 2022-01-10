0528fd880e992fcaee39737d26b43d446731a22c
sunil kumar sudhansu <sunil@klbs.in>
2022-01-10 12:39:11 +0530

    Some Changes

--- HRMS.Admin.UI/Controllers/Payroll/EmployeeDetailController.cs
+++ HRMS.Admin.UI/Controllers/Payroll/EmployeeDetailController.cs
@@ -37,7 +37,7 @@ namespace HRMS.Admin.UI.Controllers.Payroll
         }
         public async Task<IActionResult> Index()
         {
-            var response = await _IEmployeeDetailRepository.GetAllEntities(x => x.IsActive && !x.IsDeleted);
+            var response = await _IEmployeeDetailRepository.GetAllEntities(null);
             await PopulateViewBag();
             ViewBag.HeaderTitle = PageHeader.HeaderSetting["EmployeeDetailIndex"];
             return await Task.Run(() => View(ViewHelper.GetViewPathDetails("EmployeeDetail", "EmployeeDetailIndex"), response.Entities));
