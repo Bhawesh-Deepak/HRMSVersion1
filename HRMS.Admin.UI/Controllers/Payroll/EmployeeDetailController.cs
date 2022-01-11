88f197083900a87d629b9deeb22d812d271af3a1
Bhawesh Deepak <Bhawesh Deepak@DESKTOP-KB2SSTK>
2022-01-11 05:08:13 -0800

    Export To Excel

--- HRMS.Admin.UI/Controllers/Payroll/EmployeeDetailController.cs
+++ HRMS.Admin.UI/Controllers/Payroll/EmployeeDetailController.cs
@@ -1,4 +1,6 @@
-﻿using HRMS.Admin.UI.Helpers;
+﻿using ClosedXML.Excel;
+using Fingers10.ExcelExport.ActionResults;
+using HRMS.Admin.UI.Helpers;
 using HRMS.Core.Entities.Common;
 using HRMS.Core.Entities.Master;
 using HRMS.Core.Entities.Organisation;
@@ -8,7 +10,10 @@ using HRMS.Services.Repository.GenericRepository;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace HRMS.Admin.UI.Controllers.Payroll
@@ -45,6 +50,75 @@ namespace HRMS.Admin.UI.Controllers.Payroll
 
             return await Task.Run(() => View(ViewHelper.GetViewPathDetails("EmployeeDetail", "EmployeeDetailIndex"), response.Entities));
         }
+
+        public async Task<IActionResult> GetFilteredData(string legalEntity, string department,
+            string designation, string plName, string doj, string location, string status)
+        {
+            var response = await _IEmployeeDetailRepository.GetAllEntities(null);
+            var models = response.Entities;
+
+            if (!string.IsNullOrEmpty(legalEntity))
+            {
+                models = models.Where(x => x.LegalEntity.ToLower().Trim() == legalEntity.ToLower().Trim()).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(department))
+            {
+                models = models.Where(x => x.DepartmentName.ToLower().Trim() == department.ToLower().Trim()).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(designation))
+            {
+                models = models.Where(x => x.DesignationName.ToLower().Trim() == designation.ToLower().Trim()).ToList();
+            }
+            if (!string.IsNullOrEmpty(plName))
+            {
+                models = models.Where(x => x.PAndLHeadName.ToLower().Trim() == plName.ToLower().Trim()).ToList();
+            }
+            if (!string.IsNullOrEmpty(doj))
+            {
+                models = models.Where(x => x.JoiningDate.Date == Convert.ToDateTime(doj).Date).ToList();
+            }
+            if (!string.IsNullOrEmpty(location))
+            {
+                models = models.Where(x => x.Location.ToLower().Trim() == location.ToLower().Trim()).ToList();
+            }
+            if (!string.IsNullOrEmpty(status))
+            {
+                bool statusValue = status == "0" ? false : true;
+                models = models.Where(x => x.IsActive == Convert.ToBoolean(statusValue)).ToList();
+            }
+
+            return await Task.Run(() => PartialView(ViewHelper.GetViewPathDetails("EmployeeDetail", "EmployeeFilteredList"), models));
+        }
+
+
+        public async Task<IActionResult> GetActiveInActiveDetails(int status)
+        {
+            var response = await _IEmployeeDetailRepository.GetAllEntities(null);
+            var models = response.Entities;
+
+            bool statusValue = status == 0 ? false : true;
+            models = models.Where(x => x.IsActive == Convert.ToBoolean(statusValue)).ToList();
+
+            return await Task.Run(() => PartialView(ViewHelper.GetViewPathDetails("EmployeeDetail", "EmployeeFilteredList"), models));
+        }
+
+        public async Task<IActionResult> ExportToExcel()
+        {
+            var response = (await _IEmployeeDetailRepository.GetAllEntities(null)).Entities;
+
+            var dataTable = ListToDataTable.GetDataTableFromList<EmployeeDetail>(response.ToList());
+
+            string fileName = "Employee Directory.xlsx";
+
+            using XLWorkbook wb = new XLWorkbook();
+            wb.Worksheets.Add(dataTable);
+            using MemoryStream stream = new MemoryStream();
+            wb.SaveAs(stream);
+            return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+        }
+
         #region PrivateFields
         private async Task PopulateViewBag()
         {
