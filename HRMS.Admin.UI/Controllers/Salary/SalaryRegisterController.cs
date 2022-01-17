85ab946d28e937d11a50bb3386d1717e14f408f7
sunil kumar sudhansu <sunil@klbs.in>
2022-01-17 12:21:06 +0530

    Salary Register

---
+++ HRMS.Admin.UI/Controllers/Salary/SalaryRegisterController.cs
@@ -0,0 +1,28 @@
+﻿using HRMS.Admin.UI.Helpers;
+using HRMS.Core.Entities.Master;
+using HRMS.Core.Helpers.CommonCRUDHelper;
+using HRMS.Core.Helpers.CommonHelper;
+using HRMS.Services.Repository.GenericRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+namespace HRMS.Admin.UI.Controllers.Salary
+{
+    public class SalaryRegisterController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                return await Task.Run(() => View(ViewHelper.GetViewPathDetails("SalaryRegister", "_SalaryRegister")));
+            }
+            catch (Exception ex)
+            {
+                string template = $"Controller name {nameof(SalaryRegisterController)} action name {nameof(Index)} exceptio is {ex.Message}";
+                Serilog.Log.Error(ex, template);
+                return RedirectToAction("Error", "Home");
+            }
+        }
+    }
+}
---
+++ HRMS.Core.ReqRespVm/Response/Salary/SalaryRegisterVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Core.ReqRespVm.Response.Salary
+{
+    public class SalaryRegisterVM
+    {
+        [Required(ErrorMessage = "this field is required..")]
+        public int DaysMonth { get; set; }
+        [Required(ErrorMessage = "this field is required..")]
+        public int DaysYear { get; set; }
+        public string EmployeeCode { get; set; }
+    }
+}
