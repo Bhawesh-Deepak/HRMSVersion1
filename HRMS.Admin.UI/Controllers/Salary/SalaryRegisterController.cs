b82c985eae4b2d0c6f27f3e935c6ad1fb617d103
sunil kumar sudhansu <sunil@klbs.in>
2022-01-19 12:02:22 +0530

    c

--- HRMS.Admin.UI/Controllers/Salary/SalaryRegisterController.cs
+++ HRMS.Admin.UI/Controllers/Salary/SalaryRegisterController.cs
@@ -40,7 +40,7 @@ namespace HRMS.Admin.UI.Controllers.Salary
             }
             catch (Exception ex)
             {
-                string template = $"Controller name {nameof(SalaryRegisterController)} action name {nameof(Index)} exceptio is {ex.Message}";
+                string template = $"Controller name {nameof(SalaryRegisterController)} action name {nameof(Index)} exception is {ex.Message}";
                 Serilog.Log.Error(ex, template);
                 return RedirectToAction("Error", "Home");
             }
