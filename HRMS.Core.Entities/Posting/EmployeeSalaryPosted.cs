e111ddd2c4769d433f473a95d720556269ca8366
sunil kumar sudhansu <sunil@klbs.in>
2022-01-20 16:49:18 +0530

    Employee Salary Posting

---
+++ HRMS.Core.Entities/Posting/EmployeeSalaryPosted.cs
@@ -0,0 +1,23 @@
+﻿using HRMS.Core.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Core.Entities.Posting
+{
+    [Table("EmployeeSalaryPosted", Schema = "Payroll")]
+    public class EmployeeSalaryPosted : BaseModel<int>
+    {
+        public int DateMonth { get; set; }
+        public int DateYear { get; set; }
+        public string EmpCode { get; set; }
+        public int ComponentId { get; set; }
+        public decimal SalaryAmount { get; set; }
+        public string LegalEntity { get; set; }
+        public string Department { get; set; }
+        public string DesignationId { get; set; }
+    }
+}
