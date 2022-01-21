ddb1f715a8cfe8d743600553719ae0c6913cbba2
azimbari1111 <azimbari1111@gmail.com>
2022-01-21 14:14:01 +0530

    Icon menu

--- HRMS.Admin.UI/Controllers/UserManagement/ModuleMasterController.cs
+++ HRMS.Admin.UI/Controllers/UserManagement/ModuleMasterController.cs
@@ -18,8 +18,17 @@ namespace HRMS.Admin.UI.Controllers.UserManagement
         }
         public IActionResult Index()
         {
-            ViewBag.HeaderTitle = PageHeader.HeaderSetting["ModuleMaster"];
-            return View(ViewHelper.GetViewPathDetails("Module", "ModuleIndex"));
+            try
+            {
+                ViewBag.HeaderTitle = PageHeader.HeaderSetting["ModuleMaster"];
+                return View(ViewHelper.GetViewPathDetails("Module", "ModuleIndex"));
+            }
+            catch (Exception ex)
+            {
+                string template = $"Controller name {nameof(ModuleMaster)} action name {nameof(Index)} exception is {ex.Message}";
+                Serilog.Log.Error(ex, template);
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         [HttpGet]
@@ -32,6 +41,8 @@ namespace HRMS.Admin.UI.Controllers.UserManagement
             }
             catch (Exception ex)
             {
+                string template = $"Controller name {nameof(ModuleMaster)} action name {nameof(GetModuleDetail)} exception is {ex.Message}";
+                Serilog.Log.Error(ex, template);
                 return RedirectToAction("Error", "Home");
             }
 
@@ -40,45 +51,72 @@ namespace HRMS.Admin.UI.Controllers.UserManagement
         [HttpGet]
         public async Task<IActionResult> CreateModule(int id)
         {
-            if (id == 0)
+            try
             {
-                return await Task.Run(() => PartialView(ViewHelper.GetViewPathDetails("Module", "ModuleCreate")));
+                if (id == 0)
+                {
+                    return await Task.Run(() => PartialView(ViewHelper.GetViewPathDetails("Module", "ModuleCreate")));
+                }
+                var response = await _IModuleMasterRepository.GetAllEntities(x => x.Id == id);
+                if (response.ResponseStatus == Core.Entities.Common.ResponseStatus.Success)
+                {
+                    return PartialView(ViewHelper.GetViewPathDetails("Module", "ModuleCreate"), response.Entities.First());
+                }
+                return RedirectToAction("Error", "Home");
             }
-            var response = await _IModuleMasterRepository.GetAllEntities(x => x.Id == id);
-            if (response.ResponseStatus == Core.Entities.Common.ResponseStatus.Success)
+            catch (Exception ex)
             {
-                return PartialView(ViewHelper.GetViewPathDetails("Module", "ModuleCreate"), response.Entities.First());
+                string template = $"Controller name {nameof(ModuleMaster)} action name {nameof(CreateModule)} exception is {ex.Message}";
+                Serilog.Log.Error(ex, template);
+                return RedirectToAction("Error", "Home");
             }
-            return RedirectToAction("Error", "Home");
         }
 
         [HttpPost]
         public async Task<IActionResult> UpsertModule(ModuleMaster model)
         {
-            if (model.Id == 0)
+            try
             {
-                var response = await _IModuleMasterRepository.CreateEntity(model);
-                return Json(new DBResponseHelper<ModuleMaster, int>().GetDBResponseHelper(response).message);
-            }
+                if (model.Id == 0)
+                {
+                    var response = await _IModuleMasterRepository.CreateEntity(model);
+                    return Json(new DBResponseHelper<ModuleMaster, int>().GetDBResponseHelper(response).message);
+                }
 
-            var updateResponse = await _IModuleMasterRepository.UpdateEntity(model);
-            return Json(new DBResponseHelper<ModuleMaster, int>().GetDBResponseHelper(updateResponse).message);
+                var updateResponse = await _IModuleMasterRepository.UpdateEntity(model);
+                return Json(new DBResponseHelper<ModuleMaster, int>().GetDBResponseHelper(updateResponse).message);
+            }
+            catch (Exception ex)
+            {
+                string template = $"Controller name {nameof(ModuleMaster)} action name {nameof(UpsertModule)} exception is {ex.Message}";
+                Serilog.Log.Error(ex, template);
+                return RedirectToAction("Error", "Home");
+            }
         }
 
         public async Task<IActionResult> DeleteModule(int id)
         {
-            var deleteModelResponse = await _IModuleMasterRepository.GetAllEntityById(x => x.Id == id);
-            if (deleteModelResponse.ResponseStatus == Core.Entities.Common.ResponseStatus.Success)
+            try
             {
-                deleteModelResponse.Entity.IsActive = false;
-                deleteModelResponse.Entity.IsDeleted = true;
+                var deleteModelResponse = await _IModuleMasterRepository.GetAllEntityById(x => x.Id == id);
+                if (deleteModelResponse.ResponseStatus == Core.Entities.Common.ResponseStatus.Success)
+                {
+                    deleteModelResponse.Entity.IsActive = false;
+                    deleteModelResponse.Entity.IsDeleted = true;
 
-                var deleteResponse = await _IModuleMasterRepository.DeleteEntity(deleteModelResponse.Entity);
+                    var deleteResponse = await _IModuleMasterRepository.DeleteEntity(deleteModelResponse.Entity);
 
-                return Json(new DBResponseHelper<ModuleMaster, int>().GetDBResponseHelper(deleteResponse));
-            }
+                    return Json(new DBResponseHelper<ModuleMaster, int>().GetDBResponseHelper(deleteResponse));
+                }
 
             return Json($"The ModuleId Id {id} you have passed is not valid !!!");
+            }
+            catch (Exception ex)
+            {
+                string template = $"Controller name {nameof(ModuleMaster)} action name {nameof(DeleteModule)} exception is {ex.Message}";
+                Serilog.Log.Error(ex, template);
+                return RedirectToAction("Error", "Home");
+            }
         }
     }
 }
