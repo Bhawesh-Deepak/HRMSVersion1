3f01cf55b6ede45da7c90cc44cc0fdc7a55e1c95
azimbari1111 <azimbari1111@gmail.com>
2022-01-25 20:04:52 +0530

    master update

---
+++ HRMS.Core.Entities/HR/PaidRegister.cs
@@ -0,0 +1,16 @@
+﻿using HRMS.Core.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Core.Entities.HR
+{
+    [Table("PaidRegister", Schema = "HR")]
+    public class PaidRegister : BaseModel<int>
+    {
+
+    }
+}
