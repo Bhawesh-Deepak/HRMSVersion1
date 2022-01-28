ee62a175ac000a3b321c767d555e15da028d3088
azimbari1111 <azimbari1111@gmail.com>
2022-01-28 10:43:32 +0530

    PaidRegisterupload

--- HRMS.Core.Entities/HR/PaidRegister.cs
+++ HRMS.Core.Entities/HR/PaidRegister.cs
@@ -1,6 +1,7 @@
 ﻿using HRMS.Core.Entities.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,13 @@ namespace HRMS.Core.Entities.HR
     [Table("PaidRegister", Schema = "HR")]
     public class PaidRegister : BaseModel<int>
     {
+        [Required(ErrorMessage = "this field is required.")]
+        [Display(Prompt = "Month")]
+        public int DateMonth{ get; set; }
 
+        [Required(ErrorMessage = "this field is required.")]
+        [Display(Prompt = "Year")]
+        public int DateYear{ get; set; }
+        public string UploadFilePath{ get; set; }
     }
 }
