4c5c37002c48febfa14b4193e997ef055197d6b7
Bhawesh Deepak <Bhawesh Deepak@DESKTOP-KB2SSTK>
2022-01-28 06:29:08 -0800

    BreadCrumb

---
+++ HRMS.Admin.UI/Controllers/Reporting/ESIController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMS.Admin.UI.Controllers.Reporting
+{
+    public class ESIController : Controller
+    {
+        private readonly IHostingEnvironment _IHostingEnviroment;
+        public ESIController(IHostingEnvironment hostingEnviroment)
+        {
+            _IHostingEnviroment = hostingEnviroment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HRMS.Core.ReqRespVm/RequestVm/ESICModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Core.ReqRespVm.RequestVm
+{
+    public class ESICModel
+    {
+        public string ESICNew { get; set; }
+        public string EmployeeName { get; set; }
+        public string ExitDate { get; set; }
+        public decimal WorkingDays { get; set; }
+        public decimal ESIAmount { get; set; }
+        public decimal GrossAmount { get; set; }
+        public int Reason { get; set; }
+    }
+}
