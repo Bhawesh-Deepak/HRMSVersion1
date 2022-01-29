fd553afa4248a47686ccfbbdbed43968a88465fb
sunil kumar sudhansu <sunil@klbs.in>
2022-01-29 16:54:19 +0530

    LWF and P tax report

---
+++ HRMS.Core.ReqRespVm/Response/Reporting/LWFReportVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Core.ReqRespVm.Response.Reporting
+{
+    public class LWFReportVM
+    {
+        public string EmpCode { get; set; }
+        public string EmployeeName { get; set; }
+        public decimal SalaryAmount { get; set; }
+        public string MonthsName { get; set; }
+        public int DateYear { get; set; }
+        public string PTStateName { get; set; }
+    }
+}
