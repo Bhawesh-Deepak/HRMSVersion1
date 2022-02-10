277fd69387dece98e762401c89b480789d7f5fb9
sunilklbs1990 <89584116+sunilklbs1990@users.noreply.github.com>
2022-02-10 11:46:40 +0530

    sunil

--- HRMS.Core.Entities/Master/Department.cs
+++ HRMS.Core.Entities/Master/Department.cs
@@ -5,16 +5,15 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace HRMS.Core.Entities.Master
 {
     [Table("Department", Schema = "Master")]
-    public class Department: BaseModel<int>
+    public class Department : BaseModel<int>
     {
-        [Required(ErrorMessage ="This field is required.")]
-        [Display(Prompt ="Department Name")]
+        [Required(ErrorMessage = "This field is required.")]
+        public int BranchId { get; set; }
+        [Required(ErrorMessage = "This field is required.")]
+        [Display(Prompt = "Department Name")]
         public string Name { get; set; }
-
+        [Required(ErrorMessage = "This field is required.")]
         [Display(Prompt = "Code")]
         public string Code { get; set; }
-
-        [Display(Prompt = "Description")]
-        public string Description { get; set; }
     }
 }
---
+++ HRMS.Core.ReqRespVm/Response/Master/DepartmentAndDesignationVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Core.ReqRespVm.Response.Master
+{
+    public class DepartmentAndDesignationVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int BranchId { get; set; }
+        public string BranchName { get; set; }
+        public string BranchCode { get; set; }
+        public int DesignationId { get; set; }
+        public string DesignationName { get; set; }
+        public string DesignationCode { get; set; }
+        
+    }
+}
