398a09646295faecf7c1babdfd17ccd6a8d67b8e
sunilklbs1990 <89584116+sunilklbs1990@users.noreply.github.com>
2022-03-07 16:59:10 +0530

    Employee Profile

---
+++ HRMS.API/Controllers/EmployeeMasterDetails/EmployeeMasterController.cs
@@ -0,0 +1,37 @@
+﻿using HRMS.Core.Entities.Payroll;
+using HRMS.Core.ReqRespVm.SqlParams;
+using HRMS.Services.Implementation.SqlConstant;
+using HRMS.Services.Repository.GenericRepository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMS.API.Controllers.EmployeeMasterDetails
+{
+    [Route("HRMS/V1/[controller]/[action]")]
+    [ApiController]
+    public class EmployeeMasterController : ControllerBase
+    {
+        private readonly IDapperRepository<EmployeeSingleDetailParam> _IEmployeeSingleDetailRepository;
+        public EmployeeMasterController(IDapperRepository<EmployeeSingleDetailParam> EmployeeSingleDetailRepository)
+        {
+            _IEmployeeSingleDetailRepository = EmployeeSingleDetailRepository;
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        [Consumes("application/json")]
+        public async Task<IActionResult> EmployeeProfile(int Id)
+        {
+            var empParams = new EmployeeSingleDetailParam()
+            {
+                Id = Id
+            };
+            var response = _IEmployeeSingleDetailRepository.GetAll<EmployeeDetail>(SqlQuery.GetEmployeeSingleDetails, empParams);
+            return Ok(response);
+        }
+    }
+}
